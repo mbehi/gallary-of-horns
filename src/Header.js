fed8f05d29ce531520e42cf6b8ee4c3d87777426
mbehi <mohsin.behi@gmail.com>
2021-03-24 18:28:51 -0500

    updated code to complete labs

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render () {
+    return (<header><nav><h1>Gallery of Horns</h1></nav></header>)
+  }
+}
+
+export default Header;
